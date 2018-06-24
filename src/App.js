52b464cbcc8a3786ada9485316865546b3ec332a
EmmaNem <emmabv1@gmail.com>
2018-06-23 22:32:10 -0700

    complete game

--- src/App.js
+++ src/App.js
@@ -15,8 +15,12 @@ class App extends Component {
     correct: []
   };
 
-  shuffleArray(array) {
-    let duplicated = array.map(function(item){return [item, item];}).reduce(function(a, b) {return a.concat(b)});
+  duplicate = () => {
+    const duplicated = this.state.paintings.map(function(item){return [item, item];}).reduce(function(a, b) {return a.concat(b)});
+    this.setState({paintings:duplicated});
+  };
+
+  shuffleArray(duplicated) {
     for (var i = duplicated.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = duplicated[i];
@@ -26,15 +30,25 @@ class App extends Component {
     return duplicated;  
   };
 
+  shuffle = () => {
+    const picture = this.shuffleArray(this.state.paintings);
+    this.setState({paintings:picture});
+  };
+
+  componentDidMount() {
+    this.duplicate();
+    setTimeout(()=>{this.shuffle()},100);
+  }
+
   game = (index) => {
     if(this.state.click === 0){
-      this.setState({click:1})
+      this.setState({click:1});
       this.setState({img1:index},function(){
         console.log(this.state.paintings[this.state.img1].name);
       });
     }
     else{
-      this.setState({click:0})
+      this.setState({click:0});
       this.setState({img2:index}, function(){
         console.log(this.state.paintings[this.state.img2].name);
         if (this.state.paintings[this.state.img1].name === this.state.paintings[this.state.img2].name) {
@@ -42,43 +56,28 @@ class App extends Component {
           let correct = this.state.correct;
           correct.push(this.state.paintings[this.state.img1].id)
           this.setState({img1: -1, img2:-1, correct:correct});
+          if ((this.state.correct).length === 6) {
+            setTimeout(()=>{this.setState({paintings:this.shuffleArray(this.state.paintings), click: 0, img1: -1, img2: -1, correct: []})},1300);
+          }
         }
         else {
           console.log("not the same");
           setTimeout(()=>{this.setState({img1: -1, img2:-1})}, 800);
         }
-
       });
-
-      
     }
-
-    /*
-    this.setState({img:index}, function(){
-      console.log(this.state.paintings[this.state.img].name);
-    })*/
-    
-  }
-
-  shuffle = () => {
-    const picture = this.shuffleArray(this.state.paintings);
-    this.setState({paintings:picture});
-  };
-
-  componentDidMount() {
-    this.shuffle();
   }
 
   render() {
-    //console.log(this.state.paintings)
-    //console.log(this.state.img)
     return (
       <Wrapper>
         <Title>Art Gallery</Title>
         {this.state.paintings.map((painting, i) => (
           <Painting
+            duplicate={painting.duplicate}
             shuffle={painting.shuffle}
             game={this.game}
+            win={this.win}
             id={painting.id}
             key={i}
             index={i}
--- src/components/Painting/Painting.js
+++ src/components/Painting/Painting.js
@@ -3,29 +3,18 @@ import "./Painting.css";
 
 class  Painting extends Component {
 
-  
-  state = {
-    pic: this.props.image
-  };
-
-  /*
-  flip = (num) => {
-    const otherpic = this.props.image;
-    //console.log("clicky");
-    num = this.props.id;
-    //console.log(this.props.id)
-    this.setState({pic:otherpic});
-  };*/
-
   render() {
+    let flippy = this.props.index;
+    let front = this.props.image;
+    let back = "https://cdn.pixabay.com/photo/2016/06/27/14/57/palette-1482678_1280.png";
     return(
-    <div className="card">
-    <div className="img-container" onClick={()=>{this.props.game(this.props.index)}}>
-      <img alt={this.props.name} src={this.props.img1 === this.props.index || this.props.img2 === this.props.index  || this.props.correct.includes(this.props.id)? this.props.image: "https://cdn.pixabay.com/photo/2016/06/27/14/57/palette-1482678_1280.png"} />
-    </div>
-  </div>
-    )}
-  
+      <div className="card">
+      <div className="img-container" onClick={()=>{this.props.game(flippy)}}>
+        <img alt={this.props.name} src={this.props.img1 === flippy || this.props.img2 === flippy  || this.props.correct.includes(this.props.id)? front:back} />
+      </div>
+      </div>
+    );
+  }
 }
 
 export default Painting;
